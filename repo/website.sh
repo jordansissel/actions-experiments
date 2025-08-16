a05637ccfa1da42e4fe9ae0911b510c327ec98f2
Jordan Sissel <jls@semicomplete.com>
2025-08-15 22:19:19 -0700

    /workdir is owned by root?

--- repo/website.sh
+++ repo/website.sh
@@ -17,4 +17,7 @@ cd "$html"
 # Change the jekyll uid to match ours?
 # The jekyll in $PATH in this docker image will suexec to 'jekyll' which may not match our uid.
 set -x
+echo "Runner workdir:"
+ls -ld $workdir
+echo "Docker"
 docker run --volume "$PWD:/srv/jekyll:z" --volume "$workdir:/workdir:z" jekyll/minimal sh -xc "ls -ld /workdir; ls -l /workdir; usermod -u $(id -u) jekyll; jekyll build -d /workdir --disable-disk-cache"
