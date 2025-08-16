67a38c572b1042be2570ab588037e5c689ff76fd
Jordan Sissel <jls@semicomplete.com>
2025-08-15 19:16:17 -0700

    Add repo building to the local testing tool

--- fpm/local.sh
+++ fpm/local.sh
@@ -3,13 +3,16 @@
 set -e
 
 echo "Dirname: $(dirname "$0")"
+
 if [ "$(dirname "$0")" != "." ] ; then
   echo "cd"
   cd "$(dirname "$0")"
 fi
 
+WORKDIR="${RUNNER_TEMP:-$(mktemp -d)}"
+ARTIFACTSDIR="$WORKDIR/artifacts"
 
-export WORKDIR="${RUNNER_TEMP:-$(mktemp -d)}"
+mkdir "$ARTIFACTSDIR"
 
 build_images() {
   # Build all the linux packages
@@ -19,20 +22,23 @@ build_images() {
 extract_packages() {
   #jq < "$WORKDIR/metadata.json" -r 'to_entries[] | "\(.key) \(.value["containerimage.digest"])"' \
   jq < "$WORKDIR/metadata.json" -r 'to_entries[] | select(.value["containerimage.digest"] != null) | "\(.key) \(.value["containerimage.digest"])"' \
-  | xargs -P4 -n2 sh -c 'mkdir "$WORKDIR/$1" && docker run "$2" sh -c "cd target; tar -zc *" | tar -zx -C "$WORKDIR/$1"' -
+  | ARTIFACTSDIR="$ARTIFACTSDIR" xargs -P4 -n2 sh -c 'mkdir "$ARTIFACTSDIR/$1" && docker run "$2" sh -c "cd target; tar -zc *" | tar -zx -C "$ARTIFACTSDIR/$1"' -
 }
 
 cleanup() {
-  rm "$WORKDIR"/fpm-*/{fpm*,package.json}
-  rm "$WORKDIR"/metadata.json
-  rmdir "$WORKDIR"/fpm-*
-  rmdir "$WORKDIR"
+  rm "$ARTIFACTSDIR"/fpm-*/{fpm*,package.json}
+  rm "$ARTIFACTSDIR"/metadata.json
+  rmdir "$ARTIFACTSDIR"/fpm-*
+  rmdir "$ARTIFACTSDIR"
+}
+
+build_repo() {
+  [ ! -d "$WORKDIR/repo" ] && mkdir "$WORKDIR/repo"
+  sh ../repo/update.sh "$ARTIFACTSDIR" "$WORKDIR/repo"
 }
 
 build_images
-
 extract_packages
+build_repo
 
 #cleanup
-
-find "$WORKDIR"
