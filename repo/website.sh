2b3e24ada0f25776cb175d7bf3e0b1daf728b638
Jordan Sissel <jls@semicomplete.com>
2025-08-15 23:14:31 -0700

    Cleanup unused

--- repo/website.sh
+++ repo/website.sh
@@ -14,13 +14,5 @@ destination="$2"
 [ -z "$source" ] && fail "Missing argument for source directory"
 [ -z "$destination" ] && fail "Missing argument for work directory"
 
-docker run -i --volume "$source:/source:z" --volume "$destination:/destination:z" jekyll/minimal \
-  sh -c "usermod -u $(id -u) jekyll; jekyll build -s /source -d /destination --disable-disk-cache"
-# docker run -i --volume "$source:/source:z" --volume "$destination:/destination:z" jekyll/minimal sh -x <<SHELL
-# ls -ld /source
-# ls -ld /destination
-
-# usermod -u $(id -u) jekyll
-
-# jekyll build -s /source -d /destination --disable-disk-cache
-# SHELL
+docker run --volume "$source:/source:z" --volume "$destination:/destination:z" jekyll/minimal \
+  sh -xc "usermod -u $(id -u) jekyll; find /source; jekyll build -s /source -d /destination --disable-disk-cache"
