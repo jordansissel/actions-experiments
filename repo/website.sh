8732f54c57c23dab0e35b0e483bc03c7464898d7
Jordan Sissel <jls@semicomplete.com>
2025-08-15 22:24:35 -0700

    Well. Lesson learned.
    Docker's --volume /no/such/path:/whatever ... will
    * Have /whatever owned by root
    * *Create* /no/such/path on the host.
    
    Unexpected.

--- repo/website.sh
+++ repo/website.sh
@@ -11,13 +11,9 @@ workdir="$1"
 [ -z "$workdir" ] && fail "Missing argument for work directory"
 
 base="$(dirname "$0")"
-html="$(dirname "$0")"/../html
+html="$(dirname "$base")"/html
 
-cd "$html"
-# Change the jekyll uid to match ours?
-# The jekyll in $PATH in this docker image will suexec to 'jekyll' which may not match our uid.
 set -x
-echo "Runner workdir:"
-ls -ld $workdir
-echo "Docker"
-docker run --volume "$PWD:/srv/jekyll:z" --volume "$workdir:/workdir:z" jekyll/minimal sh -xc "ls -ld /workdir; ls -l /workdir; usermod -u $(id -u) jekyll; jekyll build -d /workdir --disable-disk-cache"
+pwd
+ls -d $workdir
+docker run --volume "$PWD:/srv/jekyll:z" --volume "$workdir:/workdir:z" jekyll/minimal jekyll build -d /workdir --disable-disk-cache
