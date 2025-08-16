79409c172d4945fc5af54f49b19ba740e863ae2a
Jordan Sissel <jls@semicomplete.com>
2025-08-15 22:04:44 -0700

    It's debugging time.

--- repo/website.sh
+++ repo/website.sh
@@ -16,4 +16,4 @@ html="$(dirname "$0")"/../html
 cd "$html"
 # Change the jekyll uid to match ours?
 # The jekyll in $PATH in this docker image will suexec to 'jekyll' which may not match our uid.
-docker run --volume "$PWD:/srv/jekyll:z" --volume "$workdir:/workdir:z" jekyll/minimal sh -c "usermod -u $(id -u) jekyll; jekyll build -d /workdir --disable-disk-cache"
+docker run --volume "$PWD:/srv/jekyll:z" --volume "$workdir:/workdir:z" jekyll/minimal sh -xc "usermod -u $(id -u) jekyll; jekyll build -d /workdir --disable-disk-cache"
