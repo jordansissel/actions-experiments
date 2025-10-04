1d6ed2fa1b88b5b17bfd416b27fe5616d00da491
Jordan Sissel <jls@semicomplete.com>
2025-10-03 21:35:06 -0700

    Package changes into a tarball and then unmount everything

--- chroot/overlay.sh
+++ chroot/overlay.sh
@@ -15,13 +15,11 @@ paths="/usr /etc /var/lib"
 for path in $paths; do
   name="$(echo $path | tr -d /)"
 
-  mkdir -p "/overlay/$name"
-  mkdir "/overlay/$name/upper"
-  mkdir "/overlay/$name/work"
+  mkdir -p "/overlay/upper/$path"
+  mkdir -p "/overlay/work/$path"
+  mkdir -p "/overlay/_/$path"
 
-  mkdir -p "/overlay/_/${path}"
-
-  mount -t overlay overlay -o "lowerdir=${path},upperdir=/overlay/${name}/upper,workdir=/overlay/${name}/work" "/overlay/_/${path}"
+  mount -t overlay overlay -o "lowerdir=${path},upperdir=/overlay/upper/${path},workdir=/overlay/work/${path}" "/overlay/_/${path}"
 done
 
 mkdir /overlay/_/tmp
@@ -36,13 +34,24 @@ mount -o bind /dev/console /overlay/_/dev/console
 mkdir /overlay/_/var/cache
 mount -o bind /var/cache /overlay/_/var/cache
 
+mkdir /overlay/_/var/log
+
 # Inside docker, /etc/resolv.conf is often mounted from the outside.
 if [ "$(stat -c "%d" /etc)" -ne "$(stat -c "%d" /etc/resolv.conf)" ]; then
+  MOUNT_RESOLV_CONF=1
   mount -o bind /etc/resolv.conf /overlay/_/etc/resolv.conf
 fi
 
 chroot /overlay/_ "$@"
 
-#for path in $paths; do
-#umount "/overlay/_/${path}"
-#done
+tar -zcf changes.tar.gz -C /overlay/upper/ .
+
+if [ ! -z "$MOUNT_RESOLV_CONF" ]; then
+  umount /overlay/_/etc/resolv.conf
+fi
+
+for path in /dev/pts /dev/console /dev /proc /usr /etc /var/lib /var/cache; do
+  umount "/overlay/_/$path"
+done
+
+umount /overlay
