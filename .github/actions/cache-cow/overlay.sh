56d2451e5fa5ba7884a88bdf5a412cdee1205535
Jordan Sissel <jls@semicomplete.com>
2025-10-03 22:01:36 -0700

    Try to make this thing a composite action. overlay.sh works locally in docker, maybe we'll get lucky on the first try.

--- .github/actions/cache-cow/overlay.sh
+++ .github/actions/cache-cow/overlay.sh
@@ -23,13 +23,18 @@ for path in $paths; do
 done
 
 mkdir /overlay/_/tmp
+mkdir /overlay/_/run
+
 mkdir /overlay/_/proc
 mount -t proc proc /overlay/_/proc
 
 mkdir /overlay/_/dev
 mount -o bind /dev /overlay/_/dev
 mount -o bind /dev/pts /overlay/_/dev/pts
-mount -o bind /dev/console /overlay/_/dev/console
+
+if [ -e /dev/console ]; then
+  mount -o bind /dev/console /overlay/_/dev/console
+fi
 
 mkdir /overlay/_/var/cache
 mount -o bind /var/cache /overlay/_/var/cache
@@ -44,13 +49,17 @@ fi
 
 chroot /overlay/_ "$@"
 
-tar -zcf changes.tar.gz -C /overlay/upper/ .
+tar -zcf /var/cache/cow.tar.gz -C /overlay/upper/ .
 
 if [ ! -z "$MOUNT_RESOLV_CONF" ]; then
   umount /overlay/_/etc/resolv.conf
 fi
 
-for path in /dev/pts /dev/console /dev /proc /usr /etc /var/lib /var/cache; do
+if [ -e /dev/console ]; then
+  umount /overlay/_/dev/console
+fi
+
+for path in /dev/pts /dev /proc /usr /etc /var/lib /var/cache; do
   umount "/overlay/_/$path"
 done
 
