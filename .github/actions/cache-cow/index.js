79575894e4d93235eb1072e9d27fa59d7f0ccd33
Jordan Sissel <jls@semicomplete.com>
2025-10-07 21:46:20 -0700

    Add @actions/io

--- .github/actions/cache-cow/index.js
+++ .github/actions/cache-cow/index.js
@@ -1,4 +1,5 @@
 import * as core from "@actions/core";
+import * as io from "@actions/io";
 import * as exec from "@actions/exec";
 import * as fs from "node:fs/promises";
 import * as path from "path";
