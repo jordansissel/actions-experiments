7cdc52f19ca4e49257c98c2c0d62ef84ed2b6e67
Jordan Sissel <jls@semicomplete.com>
2025-10-07 21:40:21 -0700

    javascript for fun and maybes

--- .github/actions/cache-cow/index.js
+++ .github/actions/cache-cow/index.js
@@ -1,1 +1,84 @@
-console.log(process.env);
+import * as core from "@actions/core";
+import * as exec from "@actions/exec";
+import * as fs from "node:fs/promises";
+import * as path from "path";
+
+class Cow {
+  constructor(paths) {
+    this.base = "/overlay";
+    this.root = path.join(this.base, "_");
+    this.mounts = [];
+    this.paths = paths;
+  }
+
+  async setup() {
+    this.#tmpfs(this.base);
+
+    this.#rootSymlinks();
+
+    this.paths.forEach(source_path => this.#overlay(source_path))
+  }
+
+  async teardown() {
+    this.mounts.reverse().forEach(async mount => {
+      await this.#sudo("umount", [mount]);
+    });
+  }
+
+  async #rootSymlinks() {
+    const toplevel = await fs.readdir("/", { "withFileTypes": true })
+
+    toplevel.forEach(async dirent => {
+      if (dirent.isSymbolicLink()) {
+        const target = await fs.readlink(path.join(dirent.parentPath, dirent.name));
+        fs.symlink(target, path.join(this.root, dirent.name));
+      }
+    });
+  }
+
+  async #sudo(command, args, options = {}) {
+    const cmd = [command].concat(args)
+    return exec.exec("sudo", cmd, options)
+  }
+
+  async #tmpfs(mount_point) {
+    await io.mkdirP(mount_point);
+    await this.#sudo("mount", ["-t", "tmpfs", "none", mount_point]);
+
+    this.mounts.append(mount_point);
+  }
+
+  async #overlay(source_path) {
+    const lower = source_path;
+    const upper = path.join(this.base, "upper", source_path);
+    const work = path.join(this.base, "work", source_path);
+    const mount_point = path.join(this.root, source_path)
+
+    await io.mkdirP(upper);
+    await io.mkdirP(work);
+    await io.mkdirP(mount_point);
+
+    const opts = `lowerdir=${lower},upperdir=${upper},workdir=${work}`
+    await this.#sudo("mount", ["-t", "overlay", "none", "-o", opts, mount_point]);
+    this.mounts.append(mount_point);
+  }
+} // Cow
+
+function main() {
+  //if (process.getuid() !== 0) {
+  //console.log("Rerunning as root");
+  //process.env["RUNNER_USER"] = process.getuid();
+  //return exec.exec("sudo", ["-E", process.execPath].append(process.execArgv))
+  //}
+
+  const cow = new Cow()
+  cow.setup()
+  exec.exec("mount | grep /overlay");
+  cow.teardown()
+}
+
+try {
+  main();
+} catch (error) {
+  core.setFailed(error.message);
+}
